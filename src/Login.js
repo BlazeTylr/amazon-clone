2565ec90a413f7569dbc6fb5334993369cb02315
BlazeTylr <blztylr@gmail.com>
2022-05-11 20:09:08 +0100

    added login page

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -12,15 +23,29 @@ function Login() {
         <h1>Sign in</h1>
         <form>
           <h5>E-mail</h5>
-          <input className="login__input" type="text" />
+          <input
+            className="login__input"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input className="login__input" type="password" />
-          <button className="login__signInBtn">Sign In</button>
+          <input
+            className="login__input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={signIn} className="login__signInBtn">
+            Sign In
+          </button>
           <p>
             By signing-in you agree to Amazon's Conditions of Use & Sale. Please see our Privacy
             Notice, our Cookies Notice and our Interest-Based Ads Notice.
           </p>
-          <button className="login__registerBtn">Create your Amazon account</button>
+          <button onClick={register} className="login__registerBtn">
+            Create your Amazon account
+          </button>
         </form>
       </div>
     </div>
